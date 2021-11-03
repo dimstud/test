21eca867cc8ffc963de9bc06205bf6893b3a15c0
dimstud <dimichageev@gmail.com>
2021-11-03 22:49:23 +0300

    slide-panel style mobile

--- src/common.blocks/menu/menu.js
+++ src/common.blocks/menu/menu.js
@@ -1,11 +1,13 @@
 const trigger = document.querySelector('.menu__body-drob-link');
 const block = document.querySelector('.menu__body-drop');
 
-function showBlock() {
-  trigger.addEventListener('click', (e) => {
+function showBlock(event) {
+  trigger.addEventListener(event, (e) => {
     e.preventDefault();
+    e.stopPropagation();
     block.classList.toggle('menu__body-drop-active');
   });
 }
 
-showBlock();
+showBlock('click');
+showBlock('mouseover');
