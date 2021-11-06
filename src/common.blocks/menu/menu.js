76b1df2527b6e7078dc45e260f8f2d70e167b482
dimstud <dimichageev@gmail.com>
2021-11-06 15:11:38 +0300

    menu mobile scroll

--- src/common.blocks/menu/menu.js
+++ src/common.blocks/menu/menu.js
@@ -2,6 +2,7 @@ const dropBtn = document.querySelector('.menu__body-drob-link');
 const headerBtn = document.querySelector('.header__burger');
 const dropMenu = document.querySelector('.menu__body-drop');
 const mainMenu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.menu__header-close');
 
 function showDropMenu(event, btn, block) {
   btn.addEventListener(event, (e) => {
@@ -17,5 +18,13 @@ function showBlock(event, btn, block) {
   });
 }
 
+function hideMenu(event, btn, block) {
+  btn.addEventListener(event, (e) => {
+    e.preventDefault();
+    block.classList.toggle('menu__active');
+  });
+}
+
 showDropMenu('click', dropBtn, dropMenu);
 showBlock('click', headerBtn, mainMenu);
+hideMenu('click', closeMenu, mainMenu);
